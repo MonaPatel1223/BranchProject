9e112f8dfd4314d77d0a21a161b9527e6dc0af4f
MonaPatel1223 <minalthakor1223@gmail.com>
2023-02-14 21:01:15 +0000

    Barch Project

---
+++ src/main/java/java/JavaTopic.java
@@ -0,0 +1,16 @@
+package java;
+
+public class JavaTopic {
+    public static void main(String[] arg){
+        System.out.println("Manual Topic");
+        System.out.println("Software Testing Types");
+        System.out.println("Testing Benefits");
+        System.out.println("Testing Levels");
+        System.out.println("Testing Challenges");
+        System.out.println("Models");
+        System.out.println("Lifecycle");
+        /*System.out.println("SDLC");multiline comments*/
+        //System.out.println("STLC");single line comments Before the the class name is java after i change manaul topic
+    }
+
+}
